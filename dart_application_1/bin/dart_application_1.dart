8ebbbbe6efc167189bebf2bf5442520b990e714b
culpepperd <culpepperd2490@gmail.com>
2023-07-22 10:39:35 -0400

    variables

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,8 +1,8 @@
 // import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main() {
-  print("    /|");
-  print("   / |");
-  print("  /  |");
-  print(" /___|");
+  String myName = "Daniel Culpepper";
+  int age = 33;
+  print("Hello, my name is $myName and I am $age years old.");
+
 }
