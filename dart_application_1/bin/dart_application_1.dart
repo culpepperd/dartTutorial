257aa598a5aa77be59dde8a0cccab632d3fc2d36
culpepperd <culpepperd2490@gmail.com>
2023-07-22 10:50:23 -0400

    texting string indeces

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -3,6 +3,14 @@
 void main() {
   String myName = "Daniel Culpepper";
   int age = 33;
+  bool answer = true;
   print("Hello, my name is $myName and I am $age years old.");
+  print(answer);
+  print(myName[0]);
+  print(myName[1]);
+  print(myName[2]);
+  print(myName[3]);
+  print(myName[4]);
+  print(myName[5]);
 
 }
